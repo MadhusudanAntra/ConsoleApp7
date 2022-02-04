589349d936eb64aca2343ff00bf085a663d7b3d6
sharm <sharm@LAPTOP-TALM7IV0>
2022-02-03 18:04:51 -0600

    Add project files.

---
+++ ConsoleApp7/ManageEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+     class ManageEmployee
+    {
+        EmployeeRepository employeeRepository;
+        public ManageEmployee()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+
+        public void PrintEmployee()
+        {
+            IEnumerable<Employee> employees = employeeRepository.GetDataByCondition(  x => x.City == "Sterling" );
+            foreach (Employee item in employees)
+            {
+                Console.WriteLine($"{item.Name}\t {item.Department} \t {item.State} \t {item.City}");
+            }
+        }
+        
+    }
+}
